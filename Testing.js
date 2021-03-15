edd4b39f33189aacd24b3378f9350d11f6f830d7
FareedFz <78958186+FareedFz@users.noreply.github.com>
2021-03-15 19:07:08 +0530

    Commit

--- Testing.js
+++ Testing.js
@@ -10,7 +10,7 @@
 // });
 var chai=require('chai');
 let chaihttp=require("chai-http");
-let server=require('../Crud');
+let server=require('../CrudArray');
 chai.should();
 chai.use(chaihttp);
  describe("task api",()=>{
@@ -21,11 +21,24 @@ chai.use(chaihttp);
                  .get('/')
                  .end((err,response)=>{
                      response.should.have.status(200)
+                     
                 done();
                  })
          })
+         it("404 found",(done)=>{
+            //chai.request(server)  //server is api
+                chai.request(server)
+                .get('/s')
+                .end((err,response)=>{
+                    response.should.have.status(404)
+               done();
+                })
+        })
 
-     })
+ })
+    
+       
+    
      describe("/get with id",()=>{
         it("its should get method with id",(done)=>{
             const takenid=1
@@ -40,40 +53,40 @@ chai.use(chaihttp);
     describe("/post method",()=>{
         it("its should post method with id",(done)=>{
             const task = {
-                order: 5
+                name: "kevin"
                         };
             chai.request(server)//server is api
                 .post('/')
                 .send(task)
                 .end((err,response)=>{
                     response.should.have.status(201);
-                    response.body.should.have.property('order').eq(6);
+                    response.body.should.have.property('name').eq("kevin");
                     done();
                 })
         })
     })
-    describe("/put method",()=>{
-        it("its should put method with id",(done)=>{
-            const taskId = 1;
-            const task1 = {
-                title: 'Have some dinner ',
-                order:1,
-                completed:'false'
-            };
-            chai.request(server)//server is api
-                .put('/'+taskId)
-                .send(task1)
-                .end((err,response)=>{
-                    response.should.have.status(200);
-                    response.body.should.have.property('title').eq('Have some dinner');
-                    response.body.should.have.property('order').eq(1);
-                    response.body.should.have.property('completed').eq('false');
+    // describe("/put method",()=>{
+    //     it("its should put method with id",(done)=>{
+    //         const taskId = 1;
+    //         const task1 = {
+    //             title: 'Have some dinner ',
+    //             order:1,
+    //             completed:'false'
+    //         };
+    //         chai.request(server)//server is api
+    //             .put('/'+taskId)
+    //             .send(task1)
+    //             .end((err,response)=>{
+    //                 response.should.have.status(200);
+    //                 response.body.should.have.property('name').eq('Have some dinner');
+    //                 response.body.should.have.property('order').eq(1);
+    //                 response.body.should.have.property('completed').eq('false');
                     
 
-                    done();
-                })
-        })
-    })
+    //                 done();
+    //             })
+    //     })
+    // })
 
     describe("/delete method",()=>{
         it("its should delete method with id",(done)=>{
